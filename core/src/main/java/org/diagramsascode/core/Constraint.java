e83860b024baf8e0e10e64ce00fa471f7292ad52
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-20 16:49:57 +0100

    Add JavaDoc

--- core/src/main/java/org/diagramsascode/core/Constraint.java
+++ core/src/main/java/org/diagramsascode/core/Constraint.java
@@ -3,12 +3,23 @@ package org.diagramsascode.core;
 import java.util.Optional;
 
 /**
- * Constrains diagram elements of the specified type
+ * Constrains diagram elements of the specified diagram element type.
  * 
  * @author b_muth
  *
  * @param <T> the type to constrain
  */
 public interface Constraint<T extends DiagramElement> {
+  
+  /**
+   * Validate if the specified diagram element's information is correct,
+   * and return a constraint violation if not. Implementations first
+   * have to check whether the constraint applies to the specified
+   * diagram element (typically using instanceof), because other types
+   * of diagram elements may be passed in as argument.
+   * 
+   * @param diagramElement the element to validate
+   * @return the (optional) constraint violation, or an empty optional
+   */
   Optional<ConstraintViolation<T>> validate(T diagramElement);
 }
--- core/src/main/java/org/diagramsascode/core/ConstraintViolation.java
+++ core/src/main/java/org/diagramsascode/core/ConstraintViolation.java
@@ -2,25 +2,53 @@ package org.diagramsascode.core;
 
 import java.util.Objects;
 
+/**
+ * Class representing the violation of a constraint,
+ * when a diagram is validated.
+ * 
+ * @author b_muth
+ *
+ * @param <T> the type of diagram element for which a constraint has been violated.
+ */
 public class ConstraintViolation<T extends DiagramElement> {
   private final Constraint<T> constraint;
   private final T diagramElement;
   private final String message;
 
+  /**
+   * Creates a constraint violation.
+   * 
+   * @param constraint the constraint that has been violated.
+   * @param diagramElement the diagram element for which a constraint has been violated.
+   * @param message a message describing the violation.
+   */
   public ConstraintViolation(Constraint<T> constraint, T diagramElement, String message) {
     this.constraint = Objects.requireNonNull(constraint, "constraint must be non-null");
     this.diagramElement = Objects.requireNonNull(diagramElement, "diagramElement must be non-null");
     this.message = Objects.requireNonNull(message, "message must be non-null");
   }
 
+  /**
+   * Returns the constraint that has been violated.
+   * 
+   * @return the violated constraint
+   */
   public Constraint<T> getConstraint() {
     return constraint;
   }
 
+  /**
+   * Returns the diagram element for which a constraint has been violated.
+   * @return the diagram element
+   */
   public T getDiagramElement() {
     return diagramElement;
   }
 
+  /**
+   * Returns the message describing the violation.
+   * @return the message
+   */
   public String getMessage() {
     return message;
   }
