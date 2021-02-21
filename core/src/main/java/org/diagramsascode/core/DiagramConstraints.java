3fde9fbe12327bcab86a8b84544a0753cbdb86cf
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 07:15:45 +0100

    Add JavaDoc

--- core/src/main/java/org/diagramsascode/core/DiagramConstraints.java
+++ core/src/main/java/org/diagramsascode/core/DiagramConstraints.java
@@ -2,7 +2,28 @@ package org.diagramsascode.core;
 
 import java.util.Collection;
 
+/** 
+ * Used for collecting all constraints that are applied to a diagram.
+ * 
+ * @author b_muth
+ *
+ */
 public interface DiagramConstraints {
+  /**
+   * Specifies constraints that affect nodes on a diagram.
+   *  
+   * @param diagram the diagram whose nodes are constrained.
+   * 
+   * @return the node constraints specified for the diagram
+   */
   Collection<DiagramNodeConstraint> nodeConstraintsFor(Diagram diagram);
+  
+  /**
+   * Specifies constraints that affect edges on a diagram.
+   *  
+   * @param diagram the diagram whose edges are constrained.
+   * 
+   * @return the edge constraints specified for the diagram
+   */
   Collection<DiagramEdgeConstraint> edgeConstraintsFor(Diagram diagram);
 }
