8057200da64dce6e891cfe375d6f7f043366ea1e
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 09:17:42 +0100

    Add Javadoc for DiagramToSource class

--- image/src/main/java/org/diagramsascode/image/DiagramToSource.java
+++ image/src/main/java/org/diagramsascode/image/DiagramToSource.java
@@ -4,6 +4,12 @@ import org.diagramsascode.core.Diagram;
 import org.diagramsascode.core.DiagramEdge;
 import org.diagramsascode.core.DiagramNode;
 
+/**
+ * Interface to be implemented by converter classes that convert diagram models to text.
+ * 
+ * @author b_muth
+ *
+ */
 public interface DiagramToSource {
   String header(Diagram diagram);
   String node(DiagramNode node);
