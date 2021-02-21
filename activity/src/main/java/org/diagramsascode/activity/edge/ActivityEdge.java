c91bd6f67896bd0a1c2547584384dc83249ed111
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 08:12:03 +0100

    Add JavaDoc for activity edges

--- activity/src/main/java/org/diagramsascode/activity/edge/ActivityEdge.java
+++ activity/src/main/java/org/diagramsascode/activity/edge/ActivityEdge.java
@@ -3,11 +3,30 @@ package org.diagramsascode.activity.edge;
 import org.diagramsascode.activity.node.ActivityNode;
 import org.diagramsascode.core.DiagramEdge;
 
+/**
+ * Abstract base class for all edges shown on an activity diagram.
+ *  
+ * @author b_muth
+ *
+ */
 public abstract class ActivityEdge extends DiagramEdge {
+  /**
+   * Creates an activity edge with empty text.
+   * 
+   * @param from the starting node of the edge
+   * @param to the ending node of the edge
+   */
   public ActivityEdge(ActivityNode from, ActivityNode to) {
     this(from, to, "");
   }
   
+  /**
+   * Creates an activity edge with the specified text.
+   * 
+   * @param from the starting node of the edge
+   * @param to the ending node of the edge
+   * @param text the text of the edge
+   */
   public ActivityEdge(ActivityNode from, ActivityNode to, String text) {
     super(from, to, text);
   }
--- activity/src/main/java/org/diagramsascode/activity/edge/ControlFlow.java
+++ activity/src/main/java/org/diagramsascode/activity/edge/ControlFlow.java
@@ -4,11 +4,31 @@ import java.util.Objects;
 
 import org.diagramsascode.activity.node.ActivityNode;
 
+/**
+ * A control flow edge on an activity diagram.
+ * It connects to activity nodes.
+ * 
+ * @author b_muth
+ *
+ */
 public class ControlFlow extends ActivityEdge {
+  /**
+   * Creates a control flow edge without a condition.
+   * 
+   * @param from the starting node of the edge
+   * @param to the ending node of the edge
+   */
   public ControlFlow(ActivityNode from, ActivityNode to) {
     super(from, to);
   }
   
+  /**
+   * Creates a control flow edge with a condition.
+   * 
+   * @param from the starting node of the edge
+   * @param to the ending node of the edge
+   * @param condition the condition of the edge. Don't use brackets [] in it.
+   */
   public ControlFlow(ActivityNode from, ActivityNode to, String condition) {
     super(from, to, createGuard(condition));
   }
