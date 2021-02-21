c8b18df6d6bc35827f218b62dfaf8cdfb8c0b5f2
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 09:08:35 +0100

    Add package info for core package

---
+++ core/src/main/java/org/diagramsascode/core/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Contains the core classes of diagrams as code:
+ * diagram nodes, edges and constraints, and how to validate them.
+ * 
+ * They are not intended to be used by users directly.
+ * 
+ * Instead, the should be used by diagram specific projects that extend
+ * them to define the nodes, edges and constraints of a specific diagram. 
+ *
+ * @author b_muth
+ */
+package org.diagramsascode.core;
