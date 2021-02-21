35f68ef1f0f0a73632f62491db372c40f0dd0fc3
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 07:23:25 +0100

    Add JavaDoc for edge and node constraints

--- core/src/main/java/org/diagramsascode/core/DiagramEdgeConstraint.java
+++ core/src/main/java/org/diagramsascode/core/DiagramEdgeConstraint.java
@@ -1,4 +1,10 @@
 package org.diagramsascode.core;
 
+/**
+ * Specified a constraint for an edge on a diagram.
+ * 
+ * @author b_muth
+ *
+ */
 public interface DiagramEdgeConstraint extends Constraint<DiagramEdge>{
 }
--- core/src/main/java/org/diagramsascode/core/DiagramNodeConstraint.java
+++ core/src/main/java/org/diagramsascode/core/DiagramNodeConstraint.java
@@ -1,4 +1,10 @@
 package org.diagramsascode.core;
 
+/**
+ * Specified a constraint for a node on a diagram.
+ * 
+ * @author b_muth
+ *
+ */
 public interface DiagramNodeConstraint extends Constraint<DiagramNode>{
 }
