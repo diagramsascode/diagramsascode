cad17c32deb02b71f6eac09921ceaf7ca49651ea
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 09:13:54 +0100

    Add package infos for image subproject

---
+++ image/src/main/java/org/diagramsascode/image/activity/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains converter classes to convert activity diagram models to text.
+ *
+ * @author b_muth
+ */
+package org.diagramsascode.image.activity;
---
+++ image/src/main/java/org/diagramsascode/image/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains the classes to generate diagram images.
+ *
+ * @author b_muth
+ */
+package org.diagramsascode.image;
