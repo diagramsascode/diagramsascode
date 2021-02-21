e485567ab5f27909210c85586d7d7515dda6e993
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-21 09:16:23 +0100

    Add Javadoc for ConstraintViolationException

--- image/src/main/java/org/diagramsascode/image/ConstraintViolationException.java
+++ image/src/main/java/org/diagramsascode/image/ConstraintViolationException.java
@@ -6,15 +6,33 @@ import java.util.stream.Collectors;
 import org.diagramsascode.core.ConstraintViolation;
 import org.diagramsascode.core.DiagramElement;
 
+/**
+ * Thrown to indicate constraints have been violated when trying
+ * to generate an image from a diagram model.
+ * 
+ * @author b_muth
+ *
+ */
 @SuppressWarnings("serial")
 public class ConstraintViolationException extends RuntimeException{
   private final List<ConstraintViolation<? extends DiagramElement>> constraintViolations;
 
+  /**
+   * Creates an exception based on the constraint violations that have occured
+   * when validating the diagram.
+   * 
+   * @param constraintViolations the violations that have occured
+   */
   public ConstraintViolationException(List<ConstraintViolation<? extends DiagramElement>> constraintViolations) {
     super("Constraints violated");
     this.constraintViolations = constraintViolations;
   }
   
+  /**
+   * Returns the constraint violations contained by the exception.
+   * 
+   * @return the violations
+   */
   public List<ConstraintViolation<? extends DiagramElement>> getConstraintViolations() {
     return constraintViolations;
   }
