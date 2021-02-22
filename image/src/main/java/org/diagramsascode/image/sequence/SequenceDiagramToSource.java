0ca2a3898fbcb84ae470cf5781afc5fe136dfe89
bertilmuth <Bertil.Muth@HOOD-Group.com>
2021-02-22 20:16:02 +0100

    Implement sequence diagram

---
+++ image/src/main/java/org/diagramsascode/image/sequence/SequenceDiagramToSource.java
@@ -0,0 +1,46 @@
+package org.diagramsascode.image.sequence;
+
+import org.diagramsascode.core.Diagram;
+import org.diagramsascode.core.DiagramEdge;
+import org.diagramsascode.core.DiagramNode;
+import org.diagramsascode.image.DiagramToSource;
+
+/**
+ * Converts a sequence diagrams content to a source text
+ * that can be used by PlantUML to generate an activity diagram image.
+ * 
+ * @author b_muth
+ *
+ */
+public class SequenceDiagramToSource implements DiagramToSource {
+  static final String ASYNC_MESSAGE = " ->> ";
+  static final String QUOTE = "\"";
+  static final String DIAGRAM_HEADER = "@startuml\n!pragma graphviz_dot smetana\n" + 
+      "skinparam monochrome true\nhide empty description\n";
+  static final String DIAGRAM_FOOTER = "@enduml\n";
+    
+  @Override
+  public String header(Diagram diagram) {
+    return DIAGRAM_HEADER;
+  }
+
+  @Override
+  public String node(DiagramNode node) {
+    String source = "";
+    return source;
+  }
+
+  @Override
+  public String edge(DiagramEdge edge) {
+    String fromNodeText = edge.getFrom().getText();
+    String toNodeText = edge.getTo().getText();
+    
+    String source = QUOTE + fromNodeText + QUOTE + ASYNC_MESSAGE + QUOTE + toNodeText + QUOTE + " : " + edge.getText() + "\n";
+    return source;
+  }
+
+  @Override
+  public String footer(Diagram diagram) {
+    return DIAGRAM_FOOTER;
+  }
+}
---
+++ image/src/test/java/org/diagramsascode/image/sequence/SequenceDiagramToSourceTest.java
@@ -0,0 +1,75 @@
+package org.diagramsascode.image.sequence;
+
+import static org.diagramsascode.image.sequence.SequenceDiagramToSource.*;
+import static org.diagramsascode.image.sequence.SequenceDiagramToSource.DIAGRAM_HEADER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.diagramsascode.core.Diagram;
+import org.diagramsascode.image.ImageSource;
+import org.diagramsascode.sequence.edge.Message;
+import org.diagramsascode.sequence.node.Participant;
+import org.junit.jupiter.api.Test;
+
+class SequenceDiagramToSourceTest {
+  @Test
+  void convertsDiagramWithoutNodesAndEdgesToCode() {
+    Diagram diagram = Diagram.builder()
+      .withNodes()
+      .withEdges()
+      .build();
+
+    String expectedSource = DIAGRAM_HEADER + DIAGRAM_FOOTER;
+    String actualSource = 
+      ImageSource.ofSequenceDiagram(diagram).toString();
+    
+    assertEquals(expectedSource, actualSource);
+  }
+  
+  @Test
+  void convertsDiagramWithOneEdgeToText() {
+    final Participant from  = new Participant("From");
+    final Participant to  = new Participant("To");
+    final Message edge = new Message(from, to, "Message");
+    
+    Diagram diagram = Diagram.builder()
+      .withNodes(from, to)
+      .withEdges(edge)
+      .build();
+    
+    String expectedSource = 
+      DIAGRAM_HEADER + 
+      "\"" + from.getText() + "\" ->> \"" + to.getText() + "\" : Message\n" +
+      DIAGRAM_FOOTER;
+    
+    String actualSource = 
+        ImageSource.ofSequenceDiagram(diagram).toString();
+    
+    assertEquals(expectedSource, actualSource);
+  }
+  
+  @Test
+  void convertsDiagramWithTwoEdgesToText() {
+    final Participant from  = new Participant("First Participant");
+    final Participant to  = new Participant("Second Participant");
+    final Message edge1 = new Message(from, to, "Request Message");
+    final Message edge2 = new Message(to, from, "Response Message");
+    
+    Diagram diagram = Diagram.builder()
+      .withNodes(from, to)
+      .withEdges(edge1, edge2)
+      .build();
+    
+    String expectedSource = 
+      DIAGRAM_HEADER + 
+      "\"" + from.getText() + "\" ->> \"" + to.getText() + "\" : Request Message\n" +
+      "\"" + to.getText() + "\" ->> \"" + from.getText() +  "\" : Response Message\n" +
+      DIAGRAM_FOOTER;
+    
+    String actualSource = 
+        ImageSource.ofSequenceDiagram(diagram).toString();
+    
+    assertEquals(expectedSource, actualSource);
+    
+    System.out.println(actualSource);
+  }
+}
